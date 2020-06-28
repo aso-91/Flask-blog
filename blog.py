e19e013bcbbbee2b0ca55dc01361081f4cfd1638
Abdullah Alotaibi <binfo.aso@gmail.com>
2020-06-29 00:03:50 +0300

    Finished Flask Blog app adding authorization - show & add posts plus styled with css

--- blog.py
+++ blog.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, session, \
-    flash, redirect, url_for, g
+from flask import Flask, request, redirect, render_template, session, \
+    g, url_for, flash
+from functools import wraps
+import sqlite3
 
-# configuration
+# Configuration
+
 DATABASE = 'blog.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
@@ -9,37 +12,74 @@ SECRET_KEY = 'hard_to_guess'
 
 app = Flask(__name__)
 
-# pulls in app configuration by looking for UPPERCASE variables
+# pulls in configuration from ALL CAPS variable
+
 app.config.from_object(__name__)
 
-# function used for connecting to the database
+# function to connect to db
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/", methods=['GET', 'POST'])
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login'))
+    return wrap
+
+
+
+
+
+# helper -- Routes
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     status_code = 200
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME'] or \
-                request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid Credential. Please try again.'
+        if request.form['username'] != app.config['USERNAME'] \
+            or request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again'
             status_code = 401
         else:
-            session['logged_in'] = True
+            session["logged_in"] = True
             return redirect(url_for('main'))
-    return render_template("login.html", error=error), status_code
+    return render_template('login.html', error=error), status_code
 
-@app.route("/main")
+@app.route('/main')
+@login_required
 def main():
-    return render_template("main.html")
+    g.db = connect_db()
+    cursor = g.db.execute('SELECT * FROM posts')
+    posts = [dict(title=row[0], post=row[1]) for row in cursor.fetchall()]
+    g.db.commit()
+    g.db.close()
+    return render_template('main.html', posts=posts)
 
-@app.route("/logout")
+@app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
+@app.route('/add', methods=['POST'])
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash('you must fill in the required fields')
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('INSERT INTO posts (title, post) VALUES(?, ?)', (request.form['title'], request.form['post']))
+        g.db.commit()
+        g.db.close()
+        flash('Entry was successful..!')
+        return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
--- sql.py
+++ sql.py
@@ -1,14 +1,13 @@
 import sqlite3
 
-# create a new database if the database doesn't already exist
-with sqlite3.connect("blog.db") as connection:
-    c = connection.cursor()
+with sqlite3.connect("blog.db") as connect:
+    c = connect.cursor()
+    c.execute("""CREATE TABLE posts(title TEXT, post TEXT)""")
 
-    c.execute(""" CREATE TABLE posts(title TEXT, post TEXT)""")
+    # Insert input into posts table
 
-    # insert dummy data into the table
     c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
     c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
     c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
     c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
-    # Notice how we escaped the apostrophes in the INSERT statements.
+
